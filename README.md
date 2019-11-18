fc72bda9932b95a83c06ba8ecbe4a1a6a676dadb
Mike Doubintchik <mike.doubintchik@gmail.com>
2019-11-18 18:30:10 -0500

    Version bump v2.1.2 and name change

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# Visual Composer Multilanguage
+# WP Bakery Multilanguage
+
 Author URI: https://allurewebsolutions.com
 Plugin URI: https://vc-multilanguage.allureprojects.com/
 Tags: visual composer, multilanguage, multi-language, translate, translation
 Donate link: https://allurewebsolutions.com/product/donation
-Tested up to: 5.2.2
-Stable tag: 2.1.1
+Tested up to: 5.3
+Stable tag: 2.1.2
 
 This is an add-on plugin for WPBakery Visual Composer that adds multilanguage support and functionality. Version 2.0 now also has automatic translations that you can turn on.
 
-
 ## Description
+
 This is an add-on plugin for WPBakery Visual Composer that adds functionality of a language switcher, making it very easy to create a multi-language site without an additional multi-language or translation plugin.
 
 In version 2.0, we've added support for automatic translations using the Google Cloud Translate API.
@@ -19,18 +20,21 @@ In version 2.0, we've added support for automatic translations using the Google 
 [Demo](https://vc-multilanguage.allureprojects.com)
 
 ## Installation
+
 1) Activate the plugin. Must have Visual Composer also activated.
 2) Go to plugin settings and input the languages you would like to support.
 3) Go to VC editor and add rows. Select language from the dropdown and add a class of "language" to the extra classes section.
 4) For auto translation, you follow step 3, but leave the text-block inside the row empty.
 
 ## Screenshots
+
 1. Settings screen
 2. Edit VC row and select language from dropdown
 3. Add "language class"
 4. Full setup
 
 ## Changelog
+
 2.1: Added default language setting
 2.0: Add automatic translation functionality
 1.0.1: Update readme and screenshots
--- visual-composer-multilanguage.php
+++ visual-composer-multilanguage.php
@@ -13,10 +13,10 @@
  * @package           Visual_Composer_Multilanguage
  *
  * @wordpress-plugin
- * Plugin Name:       Visual Composer Multilanguage
+ * Plugin Name:       WP Bakery Multilanguage
  * Plugin URI:        https://allurewebsolutions.com/projects/visual-composer-multilanguage
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           2.1.1
+ * Version:           2.1.2
  * Author:            Allure Web Solutions
  * Author URI:        https://allurewebsolutions.com
  * License:           GPL-2.0+
@@ -26,7 +26,7 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
@@ -34,8 +34,9 @@ if ( ! defined( 'WPINC' ) ) {
  * The code that runs during plugin activation.
  * This action is documented in includes/class-visual-composer-multilanguage-activator.php
  */
-function activate_visual_composer_multilanguage() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-visual-composer-multilanguage-activator.php';
+function activate_visual_composer_multilanguage()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-visual-composer-multilanguage-activator.php';
 	Visual_Composer_Multilanguage_Activator::activate();
 }
 
@@ -43,19 +44,20 @@ function activate_visual_composer_multilanguage() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-visual-composer-multilanguage-deactivator.php
  */
-function deactivate_visual_composer_multilanguage() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-visual-composer-multilanguage-deactivator.php';
+function deactivate_visual_composer_multilanguage()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-visual-composer-multilanguage-deactivator.php';
 	Visual_Composer_Multilanguage_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_visual_composer_multilanguage' );
-register_deactivation_hook( __FILE__, 'deactivate_visual_composer_multilanguage' );
+register_activation_hook(__FILE__, 'activate_visual_composer_multilanguage');
+register_deactivation_hook(__FILE__, 'deactivate_visual_composer_multilanguage');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-visual-composer-multilanguage.php';
+require plugin_dir_path(__FILE__) . 'includes/class-visual-composer-multilanguage.php';
 
 /**
  * Begins execution of the plugin.
@@ -66,10 +68,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-visual-composer-multilangu
  *
  * @since    1.0.0
  */
-function run_visual_composer_multilanguage() {
+function run_visual_composer_multilanguage()
+{
 
 	$plugin = new Visual_Composer_Multilanguage();
 	$plugin->run();
-
 }
 run_visual_composer_multilanguage();
